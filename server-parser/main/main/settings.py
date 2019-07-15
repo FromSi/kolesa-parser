6b0c7e401a2a935dc7c67ef20296b1c0a9b34d06
FromSi <fromsi665@gmail.com>
2019-07-15 20:52:48 +0600

    Update

--- server-parser/main/main/settings.py
+++ server-parser/main/main/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p9ud596r1zqa*zbuuzxrne0&bq(fysdgg&1rhw41+#0n!j12w#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Flase
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.103']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.103']
 
 
 # Application definition
